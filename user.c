e30ac1489c13d8173a75808005bd7b70d0a149b1
unknown <albu.emanuel26@gmail.com>
2024-07-30 23:36:40 +0300

    new features on user side and also on menu side

--- user.c
+++ user.c
@@ -4,10 +4,10 @@
 #include"user.h"
 
 
-user initUser(const char* name, int choose) {
+user initUser(const char* name, int age) {
     user n1;
     strncpy(n1.name, name, sizeof(n1.name)-1);
-    n1.choose = choose;
+    n1.age = age;
     n1.score = 0;
 
     return n1;
@@ -17,4 +17,8 @@ void printScore(const user* n1){
 
     printf("Score is: %d", n1->score);
 
+}
+
+void incrScore(user* n1){
+    n1->score = n1->score + 1;
 }
